e819af763016df87125154116fe455a99636a206
MadVodka <ivanshblu@gmail.com>
2019-09-04 18:27:08 +0400

    code reformatted

--- src/main/java/ivan/vatlin/http/services/DemoServiceImpl.java
+++ src/main/java/ivan/vatlin/http/services/DemoServiceImpl.java
@@ -24,7 +24,7 @@ public class DemoServiceImpl implements DemoService {
     private void testPostRequest() {
         Map<String, String> headers = new HashMap<>();
         headers.put("User-Agent", "Opera/9.60 (Windows NT 6.0; U; en) Presto/2.1.1");
-        headers.put("accept","application/json");
+        headers.put("accept", "application/json");
 
         Map<String, String> parameters = new LinkedHashMap<>();
         parameters.put("name", "George");
